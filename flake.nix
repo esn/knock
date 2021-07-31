af4f8b2fa048aeced279974342bff3ec415a20d0
benton <bentonedmondson@gmail.com>
2021-07-30 19:04:06 -0500

    added 'alpha' to version

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
   in {
     defaultPackage.x86_64-linux = nixpkgs.stdenv.mkDerivation {
         pname = "knock";
-        version = "0.1.0";
+        version = "0.1.0-alpha";
         src = self;
         nativeBuildInputs = [ nixpkgs.makeWrapper ];
         buildInputs = [
