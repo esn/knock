d33a44a143662132b0e35154f841baec06b0f29a
benton <bentonedmondson@gmail.com>
2021-08-02 13:57:32 -0500

    added meta info to flake

--- flake.nix
+++ flake.nix
@@ -30,6 +30,18 @@
           cp knock $out/bin
           wrapProgram $out/bin/knock --prefix PATH : ${nixpkgs.lib.makeBinPath [libgourou-utils inept-epub]}
         '';
+
+        meta = {
+          description = "A CLI tool to convert ACSM files to DRM-free EPUB files";
+          homepage = "https://github.com/BentonEdmondson/knock";
+          license = [ nixpkgs.lib.licenses.gpl3Only ];
+          maintainers = [{
+            name = "Benton Edmondson";
+            email = "bentonedmondson@gmail.com";
+          }];
+          # potentially others, but I'm only listed those tested
+          platforms = [ "x86_64-linux" ];
+        };
       };
 
   };
